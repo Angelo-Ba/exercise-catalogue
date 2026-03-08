9f9674b37fb5c80aa07cc3c893985abed90829c6
angelo-github <angelo.vec18@gmail.com>
2026-03-08 16:53:56 +0100

    New: create components and services using command ng

--- frontend/MiniCatalogue/src/app/app.routes.ts
+++ frontend/MiniCatalogue/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductForm } from './components/product-form/product-form';
+import { ProductList } from './components/product-list/product-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductList },
+  { path: 'products/new', component: ProductForm },
+  { path: 'products/edit/:id', component: ProductForm },
+];
